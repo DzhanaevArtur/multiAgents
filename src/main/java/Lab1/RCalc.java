ec42c583da025bbe0f2a23f1ee7f4d4131e8eb83
DzhanaevArtur <janaevak@yandex.ru>
2022-09-21 19:26:07 +0300

    Ready

--- src/main/java/Lab1/RCalc.java
+++ src/main/java/Lab1/RCalc.java
@@ -26,7 +26,6 @@ public class RCalc extends Calculations implements Operation {
     }
 
     String intToRoman(int num) {
-        System.out.println("Integer: " + num);
         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] romanLetters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder roman = new StringBuilder();
