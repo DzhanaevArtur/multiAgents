3dc7e99ba5bb4d82ef8af11947943cdd7e3934cc
DzhanaevArtur <janaevak@yandex.ru>
2022-12-12 22:21:14 +0300

    Laboratory 3: fuck yeah

--- src/main/java/laboratoryWorks/lab3/behs/Receive.java
+++ src/main/java/laboratoryWorks/lab3/behs/Receive.java
@@ -1,8 +1,9 @@
 package laboratoryWorks.lab3.behs;
 
 import jade.core.Agent;
-import jade.core.behaviours.WakerBehaviour;
+import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
+import laboratoryWorks.lab3.common.Parser;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -10,19 +11,22 @@ import lombok.extern.slf4j.Slf4j;
  * @created 12.12.2022
  */
 @Slf4j
-public class Receive extends WakerBehaviour {
+public class Receive extends Behaviour {
 
     private final Agent myAgent;
 
-    public Receive(Agent myAgent, long timeout) { super(myAgent, timeout); this.myAgent = myAgent; }
+    public Receive(Agent myAgent) { super(myAgent); this.myAgent = myAgent; }
 
-    @Override protected void onWake() {
+    @Override public void action() {
         ACLMessage aclMessage = myAgent.receive();
         if (aclMessage != null) {
             log.info("\t\"{}\" was received", aclMessage.getContent());
-            if (myAgent.getLocalName().equals("Node12")) {
-                log.warn("\tCongratulations! Total length equals {}", aclMessage.getContent());
+            if (myAgent.getLocalName().equals(Parser.finish)) {
+                myAgent.doWait(500);
+                log.warn("\tCongratulations! Minimal length equals {}", aclMessage.getContent());
             }
         } else block();
     }
+
+    @Override public boolean done() { return false; }
 }
--- src/main/java/laboratoryWorks/lab3/behs/Sender.java
+++ src/main/java/laboratoryWorks/lab3/behs/Sender.java
@@ -2,7 +2,7 @@ package laboratoryWorks.lab3.behs;
 
 import jade.core.AID;
 import jade.core.Agent;
-import jade.core.behaviours.WakerBehaviour;
+import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,24 +11,26 @@ import lombok.extern.slf4j.Slf4j;
  * @created 12.12.2022
  */
 @Slf4j
-public class Sender extends WakerBehaviour {
+public class Sender extends Behaviour {
 
     private final Agent myAgent;
     private final String receiver;
     private final Integer length;
 
-    public Sender(Agent myAgent, long timeout, String receiver, Integer length) {
-        super(myAgent, timeout);
+    public Sender(Agent myAgent, String receiver, Integer length) {
+        super(myAgent);
         this.myAgent = myAgent;
         this.receiver = receiver;
         this.length = length;
     }
 
-    @Override protected void onWake() {
+    @Override public void action() {
         ACLMessage aclMessage = new ACLMessage(ACLMessage.INFORM);
         aclMessage.addReceiver(new AID(receiver, false));
         aclMessage.setContent(String.valueOf(length));
         myAgent.send(aclMessage);
         log.info("\t\"{}\" was sent to {}", aclMessage.getContent(), receiver);
     }
+
+    @Override public boolean done() { return true; }
 }
